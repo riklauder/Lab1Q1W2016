bf579b5e7003a01afade376570d0a64600b79c7c
estee <esteerixx@ESTEEXPSX>
2017-01-15 11:57:57 -0500

    fixed bug

--- Lab1W2016/Q1.c
+++ Lab1W2016/Q1.c
@@ -1,34 +1,39 @@
-/*Write a program that declares an array of 10 integers. 
-Write a loop that accepts 10 values from the keyboard, 
-and write another loop that displays the 10 values. 
+/*Write a program that declares an array of 10 integers.
+Write a loop that accepts 10 values from the keyboard,
+and write another loop that displays the 10 values.
 Do not use any subscripts within the two loops; use pointers only.
 */
 
 #include <stdio.h>
 
-int main(void) {
+main() {
 
-	//decalre array and assign pointer
-	
-	int *valuespt;
-	int i;
-	int values[i];
-	valuespt = values;
+    //decalre array and assign pointer
 
-	//loop for input of values
-	printf("input int value then enter\n");
-	for (i = 0; i < 10; i++) 
-		scanf_s("%d", (valuespt + i));
-		printf("\n");
+    int values[10]/* = { 0,1,2,3,4,5,6,7,8,9 }*/;
+    int *valuespt;
+    int i;
 
-	//display output of 10 values
-	printf("you entered:\n");
-	for (i = 0; i < 10; i++) 
-		printf("%d \n", *(valuespt + i));
+    valuespt = values;
 
-	//getchar to prvent console windows from closing
-	printf("hit enter to exit\n");
-	getchar();
+    //loop for input of values
+    printf("input int value then enter\n");
+    for (i = 0; i < 10; i++) {
+	   scanf_s("%d", (valuespt + i));
+    }
+    printf("\n");
 
-return;
+    //display output of 10 values
+    printf("you entered:\n");
+    for (i = 0; i < 10; i++) {
+	   printf("%d \n", *(valuespt + i));
+    }
+
+    /*getchar to prvent console windows from closing
+    printf("hit enter to exit");
+    getchar();
+    */
+
+    return;
+
 }
